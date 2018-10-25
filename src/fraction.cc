e6544d0857cbcbb882d16a2daa2058956ad9b7c3
Brandon Salazar <salazarbrandon11@icloud.com>
2018-10-25 09:55:04 -0700

    Push to git hub. In class fraction

---
+++ src/fraction.cc
@@ -0,0 +1,26 @@
+//
+// Created by vscilab on 10/25/2018.
+//
+#include <iostream>
+#include "fractions.h"
+
+int32_t Fraction::getNumerator() const {
+    return numerator;
+
+}
+
+int32_t Fraction::getDenominator() const {
+    return denominator;
+
+}
+void Fraction::setNumerator(int32_t numerator){
+    this-> numerator = numerator;
+
+}
+void Fraction::setDenominator(int32_t denominator) {
+    this-> denominator = denominator;
+}
+void Fraction::output() {
+    std::cout << numerator << " / " << denominator;
+
+}
---
+++ src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "fractions.h"
+int main() {
+    Fraction f(1,2);
+    f.output();
+    std::cout << std:: endl;
+
+    return 0;
+}
